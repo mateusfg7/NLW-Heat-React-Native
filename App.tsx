bbea0fd2f87756d1d9f4e27c2fb9cc12563f6351
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2021-10-21 22:15:30 -0300

    import `AppLoading` component on `App.tsx`

--- App.tsx
+++ App.tsx
@@ -4,9 +4,17 @@ import {
   Roboto_400Regular,
   Roboto_700Bold,
 } from '@expo-google-fonts/roboto'
+import AppLoading from 'expo-app-loading'
 
 import { Home } from './src/screens/Home'
 
-const App = () => <Home />
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+  })
 
-export default App
+  if (!fontsLoaded) return <AppLoading />
+
+  return <Home />
+}
