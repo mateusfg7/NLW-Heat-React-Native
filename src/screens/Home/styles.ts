0105be3047bbb3d9f92f014f1d6b2f20bc2b4fda
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2021-10-26 11:21:30 -0300

    update `Home` style
    - add padding top based on status-bar

--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -1,4 +1,5 @@
 import { StyleSheet } from 'react-native'
+import { getStatusBarHeight } from 'react-native-iphone-x-helper'
 
 import { COLORS } from '../../theme'
 
@@ -6,5 +7,6 @@ export const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: COLORS.BLACK_SECONDARY,
+    paddingTop: getStatusBarHeight() + 17,
   },
 })
